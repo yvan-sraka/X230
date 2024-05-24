a3b0115718acf3125b195a07b3753615afbb7c02
Yvan Sraka <yvan@sraka.xyz>
2024-05-24 14:02:59 +0200

    WIP

--- P3440/configuration.nix
+++ P3440/configuration.nix
@@ -90,6 +90,8 @@
   # #  wget
   # ];
 
+  services.guix.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.mtr.enable = true;
